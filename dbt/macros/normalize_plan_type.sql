c628da49e7a92dea10de4ffd677f220ed14473aa
sofillav <so.llavayol@gmail.com>
2025-06-21 15:14:54 -0300

    lower names of plan_type

--- dbt/macros/normalize_plan_type.sql
+++ dbt/macros/normalize_plan_type.sql
@@ -4,19 +4,19 @@
 
     Description:
     Standardizes mobile plan types by trimming, lowercasing, and mapping
-    common variations or typos to consistent labels: 'Control', 'Pospago', or 'Prepago'.
+    common variations or typos to consistent labels: 'control', 'pospago', or 'prepago'.
     Returns NULL if no match is found.
 
     Example:
-    {{ normalize_plan_type(' post-pago ') }} → 'Pospago'
+    {{ normalize_plan_type(' post-pago ') }} → 'pospago'
 #}
 
 {% macro normalize_plan_type(plan_type_raw) %}
   {% set cleaned_value = "lower(trim(" ~ plan_type_raw ~ "))" %}
   case
-    when {{ cleaned_value }} in ('control', 'contrrol', 'ctrl') then 'Control'
-    when {{ cleaned_value }} in ('pos', 'pospago', 'post_pago', 'post-pago') then 'Pospago'
-    when {{ cleaned_value }} in ('pre', 'pre_pago', 'prepago', 'pre-pago') then 'Prepago'
+    when {{ cleaned_value }} in ('control', 'contrrol', 'ctrl') then 'control'
+    when {{ cleaned_value }} in ('pos', 'pospago', 'post_pago', 'post-pago') then 'pospago'
+    when {{ cleaned_value }} in ('pre', 'pre_pago', 'prepago', 'pre-pago') then 'prepago'
     else null
   end
 {% endmacro %}
