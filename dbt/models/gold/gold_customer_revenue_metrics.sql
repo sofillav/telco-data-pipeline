59d008a743b440751329ebb91a6c16e4d5188f9c
sofillav <so.llavayol@gmail.com>
2025-06-22 21:41:52 -0300

    create credit_score groups

--- dbt/models/gold/gold_customer_revenue_metrics.sql
+++ dbt/models/gold/gold_customer_revenue_metrics.sql
@@ -21,9 +21,18 @@ select
         when mc.age >= 60 then 'Senior'
     end as age_group,
     mc.credit_score,
+    case
+        when mc.credit_score is null then 'Unknown'
+        when mc.credit_score < 580 then 'Poor'
+        when mc.credit_score < 670 then 'Fair'
+        when mc.credit_score < 740 then 'Good'
+        when mc.credit_score < 800 then 'Very Good'
+        when mc.credit_score <= 850 then 'Excellent'
+    end as credit_score_group,
     mc.monthly_bill_usd
 from {{ ref('silver_mobile_customers') }} mc
 left join {{ ref('silver_cities') }} ci
     on mc.city_sk = ci.city_sk
 where mc.monthly_bill_usd is not null
+
 
--- dbt/models/gold/gold_payment_behavior.sql
+++ dbt/models/gold/gold_payment_behavior.sql
@@ -9,7 +9,15 @@ select
     ph.customer_sk,
     ph.status,
     ph.payment_date,
-    mc.credit_score
+    mc.credit_score,
+    case
+        when mc.credit_score is null then 'Unknown'
+        when mc.credit_score < 580 then 'Poor'
+        when mc.credit_score < 670 then 'Fair'
+        when mc.credit_score < 740 then 'Good'
+        when mc.credit_score < 800 then 'Very Good'
+        when mc.credit_score <= 850 then 'Excellent'
+    end as credit_score_group
 from {{ ref('silver_payment_history') }} ph
 join {{ ref('silver_mobile_customers') }} mc 
-on ph.customer_sk = mc.customer_sk
+    on ph.customer_sk = mc.customer_sk
